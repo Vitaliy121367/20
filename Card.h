d43eddee2c40f57adea1afd1d606ca7d08edb340
Vitaliy <qw929496@gmail.com>
2024-06-20 18:04:17 +0300

    topupCash withdrawCash

--- Card.h
+++ Card.h
@@ -15,16 +15,17 @@ public:
 	Card();
 	Card(const char* name, double cash);
 	~Card();
-
+	//set get
 	unsigned long getcardNumber();
 	const char* getName();
 	void setExpiredDate(const char* date);
 	const char* getExpiredDate();
 	void setPIN(int oldPIN, int newPIN);
 	int getPIN();
-
 	double getCash();
 
 	void showInfo();
+	void topupCash(int pin, double mony);
+	void withdrawCash(int pin, double mony);
 };
 
